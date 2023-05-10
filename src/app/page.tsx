648084e7da6490664699de5d830b71a17963e663
Siby1lA <psb4644@gmail.com>
2023-05-10 10:57:17 +0900

    test: jotai, tailwind 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,24 @@
 'use client';
 
 import { atom, useAtom } from 'jotai';
+import { darkModeAtom } from '@/store';
 
 const countAtom = atom(0);
 
 export default function Home() {
+	const [darkMode, setDarkMode] = useAtom(darkModeAtom);
 	const [count, setCount] = useAtom(countAtom);
+
 	return (
-		<div>
-			<h1>Count: {count}</h1>
-			<button onClick={() => setCount(count + 1)}>Increase count</button>
+		<div className={`w-screen h-screen ${darkMode ? 'bg-black text-white' : 'bg-white text-black'}`}>
+			<div>
+				<h1>현재 {darkMode ? '다크' : '라이트'} 모드!</h1>
+				<button onClick={() => setDarkMode(!darkMode)}>테마 변경</button>
+			</div>
+			<div>
+				<h1>카운트: {count}</h1>
+				<button onClick={() => setCount(count + 1)}>카운트 증가</button>
+			</div>
 		</div>
 	);
 }
