17ee15f66942e31c9fc08789fce6d364f4f7aa86
bm4656 <newjin46@gmail.com>
2023-05-26 04:28:17 +0900

    refactor: Navbar 리팩토링

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,29 +6,34 @@ import { RiUser3Line, RiUser3Fill } from 'react-icons/ri';
 type Props = {
 	tab: string;
 };
-
+const icons = {
+	home: <AiOutlineHome className="text-4xl" />,
+	homeFill: <AiFillHome className="text-4xl m-1 text-primary" />,
+	music: <BsMusicPlayer className="text-4xl m-2" />,
+	musicFill: <BsFillMusicPlayerFill className="text-4xl m-2" />,
+	dm: <AiOutlineMessage className="text-4xl m-2" />,
+	dmFill: <AiFillMessage className="text-4xl m-2" />,
+	profile: <RiUser3Line className="text-4xl m-2" />,
+	profileFill: <RiUser3Fill className="text-4xl m-2" />,
+};
 export default function Navbar({ tab }: Props) {
 	return (
-		<nav className="shadow-inner text-fontGray flex justify-around items-center h-28 absolute bottom-0 left-0 w-full">
+		<nav className="shadow-inner text-zinc-400 flex justify-around items-center h-28 absolute bottom-0 left-0 w-full">
 			<Link className="text-xl flex flex-col items-center" href="/home">
-				{tab === 'home' ? <AiFillHome className="text-4xl m-1 text-primary" /> : <AiOutlineHome className="text-4xl" />}
-				홈
+				{tab === 'home' ? icons.homeFill : icons.home}
+				<span className={tab === 'home' ? 'text-primary' : ''}>홈</span>
 			</Link>
 			<Link className="text-xl flex flex-col items-center" href="/music">
-				{tab === 'music' ? (
-					<BsFillMusicPlayerFill className="text-4xl m-2" />
-				) : (
-					<BsMusicPlayer className="text-4xl m-2" />
-				)}
-				뮤직
+				{tab === 'music' ? icons.musicFill : icons.music}
+				<span className={tab === 'music' ? 'text-primary' : ''}>뮤직</span>
 			</Link>
 			<Link className="text-xl flex flex-col items-center" href="/dm">
-				{tab === 'dm' ? <AiFillMessage className="text-4xl m-2" /> : <AiOutlineMessage className="text-4xl m-2" />}
-				DM
+				{tab === 'dm' ? icons.dmFill : icons.dm}
+				<span className={tab === 'dm' ? 'text-primary' : ''}>DM</span>
 			</Link>
 			<Link className="text-xl flex flex-col items-center" href="/profile">
-				{tab === 'profile' ? <RiUser3Fill className="text-4xl m-2" /> : <RiUser3Line className="text-4xl m-2" />}
-				프로필
+				{tab === 'profile' ? icons.profileFill : icons.profile}
+				<span className={tab === 'profile' ? 'text-primary' : ''}>프로필</span>
 			</Link>
 		</nav>
 	);
