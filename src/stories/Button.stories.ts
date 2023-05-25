4394bb98508594e4fb5fbddd6aee92e982d71d52
bm4656 <newjin46@gmail.com>
2023-05-25 19:08:52 +0900

    fix: storybook 폴더 구조 변경

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Button } from './index';
+import { Button } from '../components/Button/storybook/index';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta: Meta<typeof Button> = {
