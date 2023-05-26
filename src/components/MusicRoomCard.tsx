76d4bff9b27e7f760678e288f3f8240212f6eee1
bm4656 <newjin46@gmail.com>
2023-05-27 03:07:02 +0900

    feat: 음악 방 입장

--- src/components/MusicRoomCard.tsx
+++ src/components/MusicRoomCard.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import { BsPlayFill } from 'react-icons/bs';
 import { AiFillEye } from 'react-icons/ai';
 import { CgDetailsMore } from 'react-icons/cg';
+import Link from 'next/link';
 
 type Props = {
 	musicRoom: {
@@ -17,7 +18,10 @@ export default function MusicRoomCard({ musicRoom: { title, host, view, isOwner,
 	return (
 		<article className="m-4 rounded-[40px] overflow-hidden relative w-[30rem] h-[30rem] drop-shadow-md">
 			<Image src={thumnail} alt={title} layout="fill" />
-			<div className="absolute w-[30rem] h-[30rem] bottom-0 left-0 bg-gradient-to-tl from-zinc-900" />
+			<Link
+				href={`music/${title}`}
+				className="absolute w-[30rem] h-[30rem] bottom-0 left-0 bg-gradient-to-tl from-zinc-900"
+			/>
 			<h2 className="absolute bottom-32 left-5 text-3xl font-bold text-white p-2 w-4/5">{title}</h2>
 			<h5 className="absolute bottom-[6.4rem] left-6 text-white p-2">{host}</h5>
 			<span className="absolute bottom-[1.8rem] left-8 bg-white w-16 h-16 rounded-full flex items-center justify-center">
