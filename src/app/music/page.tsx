7bd0a6b73c3cbe26f4967a7ff910d12e02b4591a
bm4656 <newjin46@gmail.com>
2023-05-28 03:38:43 +0900

    feat: MusicCategoryCard 완성 + mock 데이터 기능 완료

--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -1,18 +1,20 @@
 import MusicCarousel from '@/components/MusicCarousel';
 import Navbar from '@/components/Navbar';
 import TitleHeader from '@/components/TitleHeader';
-import { getAllRooms } from '../service/rooms';
+import { getAllCategories, getAllRooms } from '../service/rooms';
 import SearchBar from '@/components/SearchBar';
 
 export default async function MusicPage() {
 	const rooms = await getAllRooms();
+	const categories = await getAllCategories();
 	return (
-		<section>
+		<section className="overflow-y-auto absolute w-full h-full pb-28">
 			<TitleHeader title="뮤직방" />
 			<SearchBar name="뮤직방을 검색하세요..." />
 			<h2 className="text-4xl font-bold mx-5">인기</h2>
 			<MusicCarousel rooms={rooms} />
 			<h2 className="text-4xl font-bold mx-5">카테고리</h2>
+			<MusicCarousel categories={categories} />
 			<Navbar tab="music" />
 		</section>
 	);
--- src/app/service/rooms.ts
+++ src/app/service/rooms.ts
@@ -10,7 +10,17 @@ export type Room = {
 	id?: number;
 };
 
+export type Category = {
+	categoryName: string;
+	description: string;
+};
+
 export async function getAllRooms(): Promise<Room[]> {
 	const filePath = path.join(process.cwd(), 'public', 'data.json');
 	return readFile(filePath, 'utf-8').then<Room[]>(JSON.parse);
+}
+
+export async function getAllCategories(): Promise<Category[]> {
+	const filePath = path.join(process.cwd(), 'public', 'category.json');
+	return readFile(filePath, 'utf-8').then<Category[]>(JSON.parse);
 }
