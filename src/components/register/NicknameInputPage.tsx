26668db8a5b2431c3a479938984d324cb431e368
Siby1lA <psb4644@gmail.com>
2023-06-07 17:34:26 +0900

    feat: 입력바 컴포넌트화

--- src/components/register/NicknameInputPage.tsx
+++ src/components/register/NicknameInputPage.tsx
@@ -6,13 +6,14 @@ import { useState } from 'react';
 import ButtonBar from '@/components/bar/ButtonBar';
 import { SERVICE_URL } from '@/constants/ServiceUrl';
 import nicknameInput from '../../../public/lottie/nicknameInput.json';
+import InputBar from '../bar/InputBar';
 
 export default function NicknameInputPage() {
 	const router = useRouter();
-	const [nickName, setNickName] = useState<string>('');
+	const [nickname, setNickname] = useState<string>('');
 
 	const onNickNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setNickName(event.target.value);
+		setNickname(event.target.value);
 	};
 
 	// const { mutate: registerMutate } = useMutation(loginApis.getRegister, {
@@ -25,6 +26,7 @@ export default function NicknameInputPage() {
 	// TODO 회원가입 요청 보내기 {kakaoInfo, nickname}
 	const SignIn = () => {
 		// getCookie('kakaoUserInfo')
+		// removeCookie('kakaoUserInfo');
 		// registerMutate({})
 		router.push(`${SERVICE_URL.register}?page=2`);
 	};
@@ -32,16 +34,9 @@ export default function NicknameInputPage() {
 	return (
 		<div className="bg-white h-[100vh] p-[2rem] relative">
 			<h1 className="text-[2.2rem] font-bold mb-[2.3rem]">닉네임을 입력해주세요! 🔥</h1>
-			<input
-				type="text"
-				placeholder="닉네임을 입력해주세요."
-				value={nickName}
-				onChange={onNickNameChange}
-				className="w-full rounded-[0.8rem] h-[4.5rem] p-[1.5rem] mb-[15%] text-[1.4rem] bg-[#F8F8FA]"
-			/>
+			<InputBar placeholder="닉네임을 입력해주세요." value={nickname} onChange={onNickNameChange} />
 			<Lottie className="mr-[1rem]" animationData={nicknameInput} />
-			{/* TODO 닉네임 대입 API 호출 후 page2 이동 */}
-			<ButtonBar clickFn={() => SignIn()} content="다음" disabled={nickName === ''} />
+			<ButtonBar clickFn={() => SignIn()} content="다음" disabled={nickname === ''} />
 		</div>
 	);
 }
