990d5fc742833c33102205bbd37ff3989588a88b
bm4656 <newjin46@gmail.com>
2023-06-12 18:10:34 +0900

    fix: 빌드 시 토큰 로직 에러 주석처리

--- src/app/service/client.ts
+++ src/app/service/client.ts
@@ -15,46 +15,46 @@ const client = axios.create({
 	},
 });
 
-client.interceptors.response.use(
-	(res) => {
-		return res;
-	},
-	async (error) => {
-		const {
-			config,
-			response: { status },
-		} = error;
-		// 토큰이 유효하지 않을 시 그리고 ouath관련 요청이 아닐시
-		if (status === 401) {
-			try {
-				const originalRequest = config;
-				// token refresh 요청
-				const refreshToken = getCookie('refreshToken');
-				const res = await client.get('/server/user/token/reissue', { headers: { refresh: refreshToken } });
-				const newAccessToken = res?.headers['authorization']?.split(' ')[1];
-				const newRefreshToken = res?.headers['refresh']?.split(' ')[1];
-				if (newAccessToken && newRefreshToken) {
-					originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-					client.defaults.headers.Authorization = `Bearer ${newAccessToken}`;
-					setCookie('accessToken', 'accessToken', {
-						path: '/',
-						expires: accessExpires,
-					});
-					setCookie('refreshToken', 'refreshToken', {
-						path: '/',
-						expires: refreshExpires,
-					});
-				}
-				return client(originalRequest);
-			} catch (refreshError) {
-				removeCookie('accessToken');
-				removeCookie('refreshToken');
-				window.location.href = SERVICE_URL.login;
-				return Promise.reject(refreshError);
-			}
-		}
-		return Promise.reject(error);
-	}
-);
+// client.interceptors.response.use(
+// 	(res) => {
+// 		return res;
+// 	},
+// 	async (error) => {
+// 		const {
+// 			config,
+// 			response: { status },
+// 		} = error;
+// 		// 토큰이 유효하지 않을 시 그리고 ouath관련 요청이 아닐시
+// 		if (status === 401) {
+// 			try {
+// 				const originalRequest = config;
+// 				// token refresh 요청
+// 				const refreshToken = getCookie('refreshToken');
+// 				const res = await client.get('/server/user/token/reissue', { headers: { refresh: refreshToken } });
+// 				const newAccessToken = res?.headers['authorization']?.split(' ')[1];
+// 				const newRefreshToken = res?.headers['refresh']?.split(' ')[1];
+// 				if (newAccessToken && newRefreshToken) {
+// 					originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+// 					client.defaults.headers.Authorization = `Bearer ${newAccessToken}`;
+// 					setCookie('accessToken', 'accessToken', {
+// 						path: '/',
+// 						expires: accessExpires,
+// 					});
+// 					setCookie('refreshToken', 'refreshToken', {
+// 						path: '/',
+// 						expires: refreshExpires,
+// 					});
+// 				}
+// 				return client(originalRequest);
+// 			} catch (refreshError) {
+// 				removeCookie('accessToken');
+// 				removeCookie('refreshToken');
+// 				window.location.href = SERVICE_URL.login;
+// 				return Promise.reject(refreshError);
+// 			}
+// 		}
+// 		return Promise.reject(error);
+// 	}
+// );
 
 export default client;
--- src/app/service/server.ts
+++ src/app/service/server.ts
@@ -13,38 +13,38 @@ server.interceptors.request.use((config) => {
 	return config;
 });
 
-server.interceptors.response.use(
-	(res) => {
-		return res;
-	},
-	async (error) => {
-		const {
-			config,
-			response: { status },
-		} = error;
-		// 토큰이 유효하지 않을 시 그리고 ouath관련 요청이 아닐시
-		if (status === 401) {
-			try {
-				const originalRequest = config;
-				// token refresh 요청
-				const refreshToken = cookies().get('refreshToken')?.value;
-				const res = await server.get('/server/user/token/reissue', { headers: { refresh: refreshToken } });
-				const newAccessToken = res?.headers['authorization']?.split(' ')[1];
-				const newRefreshToken = res?.headers['refresh']?.split(' ')[1];
-				if (newAccessToken && newRefreshToken) {
-					originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-					server.defaults.headers.Authorization = `Bearer ${newAccessToken}`;
-					document.cookie = `accessToken=${newAccessToken}; Path=/; HttpOnly`;
-					document.cookie = `refreshToken=${newRefreshToken}; Path=/; HttpOnly`;
-				}
-				return server(originalRequest);
-			} catch (refreshError) {
-				window.location.href = SERVICE_URL.login;
-				return Promise.reject(refreshError);
-			}
-		}
-		return Promise.reject(error);
-	}
-);
+// server.interceptors.response.use(
+// 	(res) => {
+// 		return res;
+// 	},
+// 	async (error) => {
+// 		const {
+// 			config,
+// 			response: { status },
+// 		} = error;
+// 		// 토큰이 유효하지 않을 시 그리고 ouath관련 요청이 아닐시
+// 		if (status === 401) {
+// 			try {
+// 				const originalRequest = config;
+// 				// token refresh 요청
+// 				const refreshToken = cookies().get('refreshToken')?.value;
+// 				const res = await server.get('/server/user/token/reissue', { headers: { refresh: refreshToken } });
+// 				const newAccessToken = res?.headers['authorization']?.split(' ')[1];
+// 				const newRefreshToken = res?.headers['refresh']?.split(' ')[1];
+// 				if (newAccessToken && newRefreshToken) {
+// 					originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+// 					server.defaults.headers.Authorization = `Bearer ${newAccessToken}`;
+// 					document.cookie = `accessToken=${newAccessToken}; Path=/; HttpOnly`;
+// 					document.cookie = `refreshToken=${newRefreshToken}; Path=/; HttpOnly`;
+// 				}
+// 				return server(originalRequest);
+// 			} catch (refreshError) {
+// 				window.location.href = SERVICE_URL.login;
+// 				return Promise.reject(refreshError);
+// 			}
+// 		}
+// 		return Promise.reject(error);
+// 	}
+// );
 
 export default server;
