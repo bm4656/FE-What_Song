209ef0974bb5e857d0cb8c93565a5e0c79ac7ec4
bm4656 <newjin46@gmail.com>
2023-09-15 20:22:33 +0900

    feat: 채팅 임시 UI

--- src/app/chat-mock/page.tsx
+++ src/app/chat-mock/page.tsx
@@ -4,78 +4,65 @@ import React, { useEffect, useRef, useState } from 'react';
 import { CompatClient, Stomp } from '@stomp/stompjs';
 import SockJS from 'sockjs-client';
 import { RiCloudFill } from 'react-icons/ri';
-import ChattingBar from '@/components/bar/ChattingBar';
 import { getCookie } from '@/constants/cookie';
 
 type Message = { message: string };
 const emoji = ['👏', '💪', '🔥', '❤️', '🤩'];
 const mock = [{ message: 'ㅋㅋㅋㅋ' }, { message: 'good' }, { message: 'wow' }];
-export default function ChattingPage() {
+export default function ChattingMockPage() {
 	// 불러온 메세지 내역 저장
 	const [messages, setMessages] = useState<Message[]>([]);
 	// 새로 전송할 메세지 상태
 	const [newMessage, setNewMessage] = useState('');
+	// 최근의 채팅으로 스크롤 위한 ref
+	const messagesEndRef = useRef<HTMLDivElement>(null);
 
 	const mockSubscribe = () => {
 		setMessages(mock);
 	};
-	const client = useRef<CompatClient>();
 
-	const wsConnectSubscribe = () => {
-		client.current = Stomp.over(() => {
-			const sock = new SockJS('https://5a5f-2001-2d8-e2b4-1ed2-4d56-cee3-97d-d6a9.ngrok-free.app/ws-stomp');
-			return sock;
-		});
-		try {
-			client.current.connect(
-				{
-					'ngrok-skip-browser-warning': '69420',
-					token: `Bearer ${getCookie('accessToken')}`,
-				},
-				() => {
-					client.current!.subscribe(
-						// `/백엔드와 협의한 api주소/{구독하고 싶은 방의 id}`,
-						`/stream/test/post`,
-						(data) => {
-							const testMessage = JSON.parse(data.body);
-							console.log(testMessage);
-						},
-						{}
-					);
-				}
-			);
-		} catch (e) {
-			console.log(e);
-		}
-	};
 	const sendHandler = (e) => {
 		e.preventDefault();
-		// client.current!.send('/백엔드와 협의한 api주소', {}, JSON.stringify(newMessage));
-		// client.current!.send(
-		// 	`/app/post/test`,
-		// 	{
-		// 			'ngrok-skip-browser-warning': '69420',
-		// 			token: `Bearer ${getCookie('accessToken')}`,
-		// 	},
-		// 	JSON.stringify({ message: 'hi' })
-		// );
+
 		if (newMessage !== '') {
 			setMessages([...messages, { message: newMessage }]);
 			setNewMessage('');
 		}
 	};
+	const scrollToBottom = () => {
+		messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+	};
+
 	useEffect(() => {
-		wsConnectSubscribe();
+		scrollToBottom();
+	}, [messages]);
+	useEffect(() => {
 		mockSubscribe();
 	}, []);
+	useEffect(() => {
+		const receiveMessage = (msg: string) => {
+			setMessages((prevMessages) => [...prevMessages, { message: msg }]);
+		};
+
+		const interval = setInterval(() => {
+			receiveMessage(`채팅 메시지 ${messages.length + 1}`);
+		}, 2000);
+
+		return () => {
+			clearInterval(interval);
+		};
+	}, [messages]);
 	return (
 		<section>
-			<div className="w-full absolute bottom-52 p-8 h-[230px] overflow-scroll bg-scroll bg-slate-100">
-				{messages.map((message, index) => (
-					<div className="bg-blue-400 p-4 m-2 rounded-full w-fit text-white" key={index}>
-						{message.message}
-					</div>
-				))}
+			<div className="w-full absolute bottom-52 p-8 h-[230px] overflow-scroll bg-black bg-opacity-10">
+				<ul className="">
+					{messages.map((message, index) => (
+						<li className="p-1 m-2 ml-5 w-fit text-white text-xl" key={index}>
+							{message.message}
+						</li>
+					))}
+					<div ref={messagesEndRef} />
+				</ul>
 			</div>
 			<article className="flex flex-col gap-2 w-full h-44 px-10 absolute bottom-8">
 				<ul className="flex justify-evenly items-center h-20">
