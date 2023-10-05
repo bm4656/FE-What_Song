ab556d96e9d7379ef3899b32dc5e119905b73adb
bm4656 <newjin46@gmail.com>
2023-10-05 20:48:43 +0900

    fix: Next Image warn:fill 오류 수정

--- src/components/music/MusicCarousel.tsx
+++ src/components/music/MusicCarousel.tsx
@@ -19,7 +19,10 @@ export default function MusicCarousel({ rooms, categories }: Props) {
 	return (
 		<section className="flex pl-2 mb-4">
 			<ReactCarousel>
-				{rooms && rooms?.map((room: Room) => <MusicRoomCard musicRoom={room} key={room.have.musicRoomSeq} />)}
+				{rooms &&
+					rooms?.map((room, index) => (
+						<MusicRoomCard musicRoom={room} key={room.have.musicRoomSeq} priority={index < 3} />
+					))}
 				{categories &&
 					categories.map((category: Category) => <MusicCategoryCard category={category} key={category.categoryName} />)}
 			</ReactCarousel>
