f6959be88d734178567859f44ce47fea2cdb2835
bm4656 <newjin46@gmail.com>
2023-11-03 23:21:43 +0900

    feat: 친구 추가 검색 모달 토글 처리

--- src/components/profile/ProfileCard.tsx
+++ src/components/profile/ProfileCard.tsx
@@ -1,20 +1,28 @@
+'use client';
+
 import { FiSettings } from 'react-icons/fi';
+import { useAtom } from 'jotai';
 import ProfileBar from './ProfileBar';
 import SearchModal from './SearchModal';
 import ProfileButton from './ProfileButton';
+import { SearchModalAtom } from '@/state/store/searchModal';
 
 export default function ProfileCard() {
+	const [modalOpen, setModalOpen] = useAtom(SearchModalAtom);
+
 	return (
 		<section className="w-full h-[30rem]">
 			<ProfileBar />
 			<div className="flex gap-4 justify-center items-center mt-16">
 				<ProfileButton addStyle="w-[40%] text-2xl">프로필 편집</ProfileButton>
-				<ProfileButton addStyle="w-[40%] text-2xl">친구 추가</ProfileButton>
+				<ProfileButton clickFn={() => setModalOpen((prev) => !prev)} addStyle="w-[40%] text-2xl">
+					친구 추가
+				</ProfileButton>
 				<ProfileButton addStyle="w-[10%] text-3xl  flex items-center justify-center">
 					<FiSettings />
 				</ProfileButton>
 			</div>
-			<SearchModal />
+			{modalOpen && <SearchModal />}
 		</section>
 	);
 }
