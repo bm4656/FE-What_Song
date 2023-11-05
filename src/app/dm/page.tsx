45f5d4a49f6e062dcb771dcc2d546f63ebd8199c
bm4656 <newjin46@gmail.com>
2023-11-05 21:38:26 +0900

    feat: 임시 dm 페이지 생성

---
+++ src/app/dm/page.tsx
@@ -0,0 +1,7 @@
+export default function DmPage() {
+	return (
+		<section className="w-full h-full flex justify-center items-center">
+			<h1 className="text-3xl">아직 준비 중이에요...😅</h1>
+		</section>
+	);
+}
