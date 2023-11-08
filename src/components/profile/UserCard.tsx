849db9c4a8a36ae67bf27437c2561d7cb1ac9f59
bm4656 <newjin46@gmail.com>
2023-11-08 23:06:23 +0900

    feat: FollowButton 분리

--- src/components/profile/UserCard.tsx
+++ src/components/profile/UserCard.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { SimpleUser } from '@/types/user';
 import { friendApis } from '@/app/service/friend';
+import FollowButton from '../button/FollowButton';
 
 type Props = {
 	user: SimpleUser;
@@ -8,13 +9,8 @@ type Props = {
 };
 
 export default function UserCard({ user: { imgURL, nickname, email, memberSeq }, ownerSeq }: Props) {
-	const handleButton = async () => {
-		const res = await friendApis.addFriend({ ownerSeq, targetSeq: memberSeq });
-		console.log(res);
-		return res;
-	};
 	return (
-		<article className="flex w-full items-center justify-center relative my-1">
+		<article className="flex w-full min-w-full items-center justify-center relative my-1">
 			<div className="w-[90%] h-28 px-4 flex items-center cursor-pointer">
 				<div className="absolute w-[5rem] h-[5rem] p-4">
 					<Image
@@ -28,12 +24,7 @@ export default function UserCard({ user: { imgURL, nickname, email, memberSeq },
 				</div>
 				<h2 className="absolute left-[10.5rem] top-8 text-xl font-semibold truncate w-[60%] text-start">{nickname}</h2>
 				<p className="absolute left-[10.5rem] top-[3.5rem] text-xl text-neutral-400">{email}</p>
-				<button
-					onClick={handleButton}
-					className="absolute right-20 top-[2.6rem] text-xl bg-neutral-300 rounded-lg  w-20 h-9 hover:scale-105"
-				>
-					팔로우
-				</button>
+				<FollowButton followed={false} ownerSeq={ownerSeq} memberSeq={memberSeq} />
 			</div>
 		</article>
 	);
