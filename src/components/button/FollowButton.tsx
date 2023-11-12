48d5c0788bc9213f03ae080e9fb9f0d55d60f818
bm4656 <newjin46@gmail.com>
2023-11-12 17:35:32 +0900

    feat: 팔로잉, 팔로잉 취소 기능

--- src/components/button/FollowButton.tsx
+++ src/components/button/FollowButton.tsx
@@ -1,5 +1,6 @@
 // 필로우 팔로잉 상태에 따라 변하는 팔로우버튼 - 팔로우 상태에서 선택하면 팔로잉된다.
 
+import { useState } from 'react';
 import { friendApis } from '@/app/service/friend';
 
 type Props = {
@@ -9,19 +10,25 @@ type Props = {
 };
 
 export default function FollowButton({ followed = true, ownerSeq, memberSeq }: Props) {
+	const [following, setFollowing] = useState(followed);
 	const handleButton = async () => {
-		const res = await friendApis.addFriend({ ownerSeq, targetSeq: memberSeq });
-		console.log(res);
+		if (following) {
+			const res = await friendApis.unFollowFriend({ ownerSeq, targetSeq: memberSeq });
+			setFollowing(false);
+			return res;
+		}
+		const res = await friendApis.followFriend({ ownerSeq, targetSeq: memberSeq });
+		setFollowing(true);
 		return res;
 	};
 	return (
 		<button
 			onClick={handleButton}
 			className={`absolute right-20 top-[2.6rem] text-xl rounded-lg font-semibold text-white w-24 h-10 hover:scale-105 ${
-				followed ? ' bg-neutral-400' : 'bg-primary'
+				following ? ' bg-neutral-400' : 'bg-primary'
 			}`}
 		>
-			{followed ? '팔로잉' : '팔로우'}
+			{following ? '팔로잉' : '팔로우'}
 		</button>
 	);
 }
