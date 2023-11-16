3f72e41a08ae6eb063168a3fd5ebcaa5a80f23d9
bm4656 <newjin46@gmail.com>
2023-11-16 21:52:54 +0900

    fix: Title에 view 주석처리

--- src/components/TitleHeader.tsx
+++ src/components/TitleHeader.tsx
@@ -15,12 +15,12 @@ export default function TitleHeader({ title, notification, previous, isView = fa
 			{previous && <PreviousButton />}
 			<h1 className={`text-3xl font-bold ${isWrap && 'absolute top-[1.5rem]'}`}>{title}</h1>
 			{/*  알림 - api 없어서 임시로 막아둠 */}
-			{isView && (
+			{/* {isView && (
 				<span className="flex justify-center items-center absolute right-5 gap-1">
 					<AiFillEye className="text-3xl h-20" />
 					{view && <p className="text-2xl font-semibold text-zinc-400 p-1 mt-0.5">{view}</p>}
 				</span>
-			)}
+			)} */}
 		</header>
 	);
 }
