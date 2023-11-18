0d32d6dca2437a0557dc5991ef8f00e9e7275389
bm4656 <newjin46@gmail.com>
2023-11-18 23:41:08 +0900

    feat: 프로필 소유중인 뮤직방 조회

--- src/components/profile/ProfileCard.tsx
+++ src/components/profile/ProfileCard.tsx
@@ -15,7 +15,7 @@ export default function ProfileCard() {
 
 	if (!user) return null;
 	return (
-		<section className="w-full h-[30rem]">
+		<section className="w-full h-[25rem]">
 			<ProfileBar user={user} />
 			<div className="flex gap-4 justify-center items-center mt-16">
 				<ProfileButton addStyle="w-[40%] text-2xl">프로필 편집</ProfileButton>
