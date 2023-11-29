083ccd9a4cef98105544076d41343d1699319699
bm4656 <newjin46@gmail.com>
2023-11-30 00:08:19 +0900

    design: StoriesPreview 간격 조정

--- src/components/home/StoriesPreview.tsx
+++ src/components/home/StoriesPreview.tsx
@@ -34,34 +34,32 @@ export default function StoriesPreview() {
 	}
 
 	return (
-		<>
-			<div className="pb-7">
-				<Swiper slidesPerView="auto" spaceBetween={16} slidesOffsetBefore={16}>
-					<SwiperSlide style={{ width: '85px' }}>
-						<button onClick={() => router.push(`/stories/create`)} className="flex flex-col items-center ">
-							<div className="flex items-center justify-center w-[85px] h-[85px] bg-gray-200 rounded-full mb-3">
-								<BsPlusSquare className="text-4xl" />
-							</div>
-							<span className="text-xl">스토리 작성</span>
-						</button>
+		<section className="w-full shadow-sm shadow-neutral-300 mb-5 pb-3 rounded-lg min-h-[90px] overflow-x-auto">
+			<Swiper slidesPerView="auto" spaceBetween={16} slidesOffsetBefore={16}>
+				<SwiperSlide style={{ width: '80px' }}>
+					<button onClick={() => router.push(`/stories/create`)} className="flex flex-col items-center ">
+						<div className="flex items-center justify-center w-[80px] h-[80px] bg-gray-200 rounded-full mb-3">
+							<BsPlusSquare className="text-4xl" />
+						</div>
+						<span className="text-xl">스토리 작성</span>
+					</button>
+				</SwiperSlide>
+				{stories?.data.map((story: { memberName: string }, index: number) => (
+					<SwiperSlide key={index} style={{ width: '80px' }}>
+						<Link href={`/stories/${index}`} className="flex flex-col items-center">
+							<Image
+								src="https://i.pinimg.com/564x/f0/a1/9f/f0a19f453d9201c3226c2a6d4be786c0.jpg"
+								alt={`story${index}`}
+								width={85}
+								height={85}
+								style={{ objectFit: 'cover' }}
+								className="rounded-full mb-3"
+							/>
+							<span className="text-xl">{story.memberName}</span>
+						</Link>
 					</SwiperSlide>
-					{stories?.data.map((story: { memberName: string }, index: number) => (
-						<SwiperSlide key={index} style={{ width: '85px' }}>
-							<Link href={`/stories/${index}`} className="flex flex-col items-center">
-								<Image
-									src="https://i.pinimg.com/564x/f0/a1/9f/f0a19f453d9201c3226c2a6d4be786c0.jpg"
-									alt={`story${index}`}
-									width={85}
-									height={85}
-									style={{ objectFit: 'cover' }}
-									className="rounded-full mb-3"
-								/>
-								<span className="text-xl">{story.memberName}</span>
-							</Link>
-						</SwiperSlide>
-					))}
-				</Swiper>
-			</div>
-		</>
+				))}
+			</Swiper>
+		</section>
 	);
 }
