1ac8d48b84afff5911a136545db200c05fecc74f
bm4656 <newjin46@gmail.com>
2023-11-30 20:51:27 +0900

    refactor: home 뮤직 방 목록 CSR 처리

--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -1,17 +1,14 @@
 import MusicCarousel from '@/components/music/MusicCarousel';
 import TitleHeader from '@/components/TitleHeader';
-import SearchBar from '@/components/bar/SearchBar';
 import categories from '../../../public/data/category.json';
-import HydratedRooms from '@/components/home/HydratedRooms';
+import Rooms from '@/components/home/Rooms';
 
 export default async function MusicPage() {
 	return (
 		<>
 			<TitleHeader title="뮤직방" />
-			{/* <SearchBar placeholder="뮤직방을 검색하세요..." /> */}
 			<h2 className="text-4xl font-bold mx-5">인기</h2>
-			{/* @ts-expect-error Server Component */}
-			<HydratedRooms type="all" />
+			<Rooms />
 			<h2 className="text-4xl font-bold mx-5">카테고리</h2>
 			<MusicCarousel categories={categories} />
 		</>
