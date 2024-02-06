f7f4fd47ced3ae3af304aa8317850e9251a0fcd1
bm4656 <newjin46@gmail.com>
2024-02-07 00:23:53 +0900

    fix: 불필요한 페이지 제거

--- src/app/dm/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function DmPage() {
-	return (
-		<section className="w-full h-full flex justify-center items-center">
-			<h1 className="text-3xl">아직 준비 중이에요...😅</h1>
-		</section>
-	);
-}
