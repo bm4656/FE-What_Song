4fb54d27969abde4c60e2594e8acd98bb30b03c5
bm4656 <newjin46@gmail.com>
2024-02-08 01:16:12 +0900

    fix: profile 페이지 렌더링 개선

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,24 +1,19 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
 import TitleHeader from '@/components/TitleHeader';
 import PostGrid from '@/components/profile/PostGrid';
 import ProfileCard from '@/components/profile/ProfileCard';
 import useUser from '@/hooks/useUser';
-import { SERVICE_URL } from '@/constants/ServiceUrl';
 
 export default function ProfilePage() {
-	const router = useRouter();
-	const user = useUser().data;
-	useEffect(() => {
-		if (!user) router.push(`${SERVICE_URL.login}`);
-	}, [user]);
+	const { data, isLoading } = useUser();
+
+	if (isLoading) return null;
 
 	return (
 		<>
 			<TitleHeader title="프로필" />
-			{user && <ProfileCard user={user} />}
+			{data && <ProfileCard user={data} />}
 			<PostGrid />
 		</>
 	);
